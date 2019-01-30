e7e9cc452ef11950108d56063c4cc559b88be8ca
Maciej Konieczny <hello@narf.pl>
2019-01-30 18:17:20 +0100

    switched to deploying on Dokku

--- fabfile.py
+++ fabfile.py
@@ -4,14 +4,11 @@
 from __future__ import division
 from os.path import dirname, join, realpath
 
-from fabric.api import cd, env, lcd, local, run, task
-from fabric.contrib.project import rsync_project as rsync
+from fabric.api import cd, lcd, local, run, task
 
 
 CURR_DIR = dirname(realpath(__file__))
 APP_DIR = join(CURR_DIR, 'app')
-
-env.hosts = ['narf@narf.megiteam.pl']
 
 
 @task
@@ -26,53 +23,31 @@ def build_static():
         # css
         local('sass static-src/style.sass > static/style.css')
 
-@task
-def build():
-    'generate html from yaml'
-
-    with lcd(APP_DIR):
-        local('python generator.py')
-
 
 @task
 def deploy():
     'update production with latest changes'
-    # aka build, push, pull, install, rsync, restart, visit
 
-    build()
-    local('git push')
-
-    with cd('~/narf.pl/links/depl'):
-        run('git pull backup master')
-        run('source .environment && pip install -r ../env/reqs')
-
-    for subdir_name in ['build', 'static']:
-        rsync(local_dir=join(APP_DIR, subdir_name),
-              remote_dir='~/narf.pl/links/app',
-              delete=True)
-
-    restart()
+    local('git push --force-with-lease dokku HEAD:master')
     visit()
 
 
 @task
 def dev():
     'run locally'
-    build()
-    local('open http://localhost:5000/')
-    local("python '%s'" % join(APP_DIR, 'server.py'))
+
+    with lcd(APP_DIR):
+        local('python generator.py')
+        local('open http://localhost:5000/')
+        local('python server.py')
 
 
 @task
 def publish():
     'publish to GitHub and deploy to production'
+
     deploy()
-
-
-@task
-def restart():
-    'restart production'
-    run('restart-app links')
+    local('git push')
 
 
 @task
