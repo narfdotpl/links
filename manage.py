3b5108bf9123cdee7940c170e2865b58be9d420d
Maciej Konieczny <hello@narf.pl>
2022-05-17 23:49:21 +0200

    switched from Fabric to custom `manage.py`

--- manage.py
+++ manage.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from __future__ import division
+from contextlib import contextmanager
 from os.path import dirname, join, realpath
-
-from fabric.api import cd, lcd, local, run, task
+from os import system
+from sys import argv
 
 
 CURR_DIR = dirname(realpath(__file__))
 APP_DIR = join(CURR_DIR, 'app')
 LOGS_DIR = join(CURR_DIR, 'logs')
 
+TASKS = {}
+
+
+def task(func):
+    TASKS[func.__name__] = func
+    return func
+
+
+@contextmanager
+def lcd(path):
+    def local(cmd):
+        system('cd "%s" && %s' % (path, cmd))
+
+    yield local
+
 
 @task
 def build_static():
     'build js and css'
 
-    with lcd(APP_DIR):
+    with lcd(APP_DIR) as local:
         # js
         local('cp -r vendor static')
         local('coffee --print static-src/app.coffee | uglifyjs > static/app.js')
@@ -30,15 +45,15 @@ def deploy():
     'update production with latest changes'
 
     logs_fetch()
-    local('git push --force-with-lease dokku HEAD:master')
+    system('git push --force-with-lease dokku HEAD:master')
     visit()
 
 
 @task
-def dev():
+def runserver():
     'run locally'
 
-    with lcd(APP_DIR):
+    with lcd(APP_DIR) as local:
         local('python generator.py')
         local('open http://localhost:5000/')
         local('python server.py')
@@ -47,13 +62,13 @@ def dev():
 @task
 def logs_fetch():
     # logs start at the last deployment
-    with lcd(LOGS_DIR):
+    with lcd(LOGS_DIR) as local:
         local("ssh dokku -t 'docker logs $(cat /home/dokku/links/CONTAINER.web.1)' | gzip > $(date +%Y-%m-%d_%H%M).txt.gz")
 
 
 @task
 def logs_show():
-    with lcd(LOGS_DIR):
+    with lcd(LOGS_DIR) as local:
         local("gunzip -c $(ls *.txt.gz) | ag -v 'GET /static' | goaccess -o html > index.html && open index.html")
 
 
@@ -62,14 +77,14 @@ def publish():
     'publish to GitHub and deploy to production'
 
     deploy()
-    local('git push')
+    system('git push')
 
 
 @task
 def visit():
     'visit links.narf.pl'
-    local('open http://links.narf.pl/')
+    system('open http://links.narf.pl/')
 
 
 if __name__ == '__main__':
-    print 'run "fab --list"'
+    TASKS[argv[1]]()
